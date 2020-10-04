57464fc68e86bcadb8c8eae1197fc887d3a40de6
mikeku1116 <mikekuforgit@gmail.com>
2020-10-04 17:45:02 +0800

    建立首頁

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .scrapers import Klook, Kkday
 
-# Create your views here.
+
+def index(request):
+
+    klook = Klook("花蓮")
+    kkday = Kkday("花蓮")
+    tickets = klook.scrape() + kkday.scrape()
+
+    context = {
+        "tickets": tickets
+    }
+
+    return render(request, "tickets/index.html", context)
