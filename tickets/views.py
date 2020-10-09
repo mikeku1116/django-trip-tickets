ff48ccffb4fe6444b8b65adaebb28f269d355469
mikeku <mikekuforgit@gmail.com>
2020-10-10 00:10:26 +0800

    開發查詢功能

--- tickets/views.py
+++ tickets/views.py
@@ -4,12 +4,11 @@ from .scrapers import Klook, Kkday
 
 def index(request):
 
-    klook = Klook("花蓮")
-    kkday = Kkday("花蓮")
-    tickets = klook.scrape() + kkday.scrape()
+    klook = Klook(request.POST.get("city_name"))
+    kkday = Kkday(request.POST.get("city_name"))
 
     context = {
-        "tickets": tickets
+        "tickets": klook.scrape() + kkday.scrape()
     }
 
     return render(request, "tickets/index.html", context)
